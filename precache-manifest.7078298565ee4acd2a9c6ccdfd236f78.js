9966b69d2b6ec5cf91b04aa6b6377193d445ac2f
Fleshborne <blakeathompson94@gmail.com>
2020-08-13 22:43:59 -0400

    Updates

---
+++ precache-manifest.7078298565ee4acd2a9c6ccdfd236f78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba7a398094d2b570b2b5b74aac59fa2",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "997654e6184c0ecd4a95",
+    "url": "/Meme-Generator/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "a2f961f22dd295417a44",
+    "url": "/Meme-Generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "997654e6184c0ecd4a95",
+    "url": "/Meme-Generator/static/js/2.ea557584.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Meme-Generator/static/js/2.ea557584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2f961f22dd295417a44",
+    "url": "/Meme-Generator/static/js/main.1a5658c3.chunk.js"
+  },
+  {
+    "revision": "a24f7684e08eece56398",
+    "url": "/Meme-Generator/static/js/runtime-main.cc46d329.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.862d11e94f3f10fd5ab9021c9d3a47a8.js"
+  "/Meme-Generator/precache-manifest.7078298565ee4acd2a9c6ccdfd236f78.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Meme-Generator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
