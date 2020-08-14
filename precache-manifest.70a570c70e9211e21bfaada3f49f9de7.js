bdd114316bc0bb8c515b3f2039eaa038c5b8e280
Fleshborne <blakeathompson94@gmail.com>
2020-08-13 23:52:26 -0400

    Updates

---
+++ precache-manifest.70a570c70e9211e21bfaada3f49f9de7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe2bc3cc43c26c4741b4c3cd11bf352",
+    "url": "/index.html"
+  },
+  {
+    "revision": "997654e6184c0ecd4a95",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "8aeb5c91767d73dfde82",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "997654e6184c0ecd4a95",
+    "url": "/static/js/2.ea557584.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ea557584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aeb5c91767d73dfde82",
+    "url": "/static/js/main.5ea72ded.chunk.js"
+  },
+  {
+    "revision": "3ee6d9289089d700c3ad",
+    "url": "/static/js/runtime-main.2d4e65bc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Meme-Generator/precache-manifest.7078298565ee4acd2a9c6ccdfd236f78.js"
+  "/precache-manifest.70a570c70e9211e21bfaada3f49f9de7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Meme-Generator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
