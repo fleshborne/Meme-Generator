59af14ee0ca8782172dde8cee3dc1a1863a72a33
Fleshborne <blakeathompson94@gmail.com>
2020-08-13 22:39:50 -0400

    Updates

---
+++ precache-manifest.862d11e94f3f10fd5ab9021c9d3a47a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45280ad15efd176618340dd27f6d5cec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "997654e6184c0ecd4a95",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "a2f961f22dd295417a44",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "997654e6184c0ecd4a95",
+    "url": "/static/js/2.ea557584.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ea557584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2f961f22dd295417a44",
+    "url": "/static/js/main.1a5658c3.chunk.js"
+  },
+  {
+    "revision": "3ee6d9289089d700c3ad",
+    "url": "/static/js/runtime-main.2d4e65bc.js"
+  }
+]);
